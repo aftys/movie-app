6d1e5bbcbeef3bcdc7f5dbfd93c7f098d12f2228
Oussama Aftys <oussamaaftys@gmail.com>
2023-03-16 10:29:31 +0000

    walo 2

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -48,7 +48,7 @@ const RightSidebar = () => {
         <>
            
            <div className="fixed w-[280px] right-0 top-0 px-5 bg-main-dark-bg">
-                 ///////////profile
+                 {/*profile*/}
                 <div className=" h-15 flex items-center justify-around bg-black gap-4 cursor-pointer p-2 rounded-3xl">
                     <div className="rounded-full w-10 h-10 border-2 border-gray-400 overflow-hidden">
                         <img className="h-full" src={"avatar2.webp"} alt="user-profile" /> 
@@ -62,14 +62,14 @@ const RightSidebar = () => {
                     <MdKeyboardArrowDown className="text-gray-400 text-14" />
                 </div>
 
-                ///////search
+                {/*Search*/}
                 <div className="h-[40px] flex  flex-row items-center justify-start overflow-hidden bg-black gap-4 cursor-pointer p-1 rounded-3xl">
                     <p className="text-gray-400 text-3xl h-full w-[15%] flex justify-center"><MdSearch/></p>
                     <input className="bg-transparent text-center h-full  text-gray-400 w-[70%]" placeholder="Search a Movie" />
                 </div>
                 <div className='bg-main-dark-bg h-[20px] w-full'/>
             </div>
-            ////////categories
+            {/*categories*/}
             <div className="w-[90%] px-2 mt-[120px]">
                 <p className="text-gray-300 text-lg">Categories</p>
                 <div className="grid grid-cols-2 gap-3 w-full mt-5">
@@ -84,11 +84,11 @@ const RightSidebar = () => {
                     ))}
                 </div>
             </div>            
-            ////////Recent download
+            {/*Recent downolads*/}
             <div className="w-[90%] px-2 ">
                 <p className="text-gray-300 text-lg">Recent downloads</p>
-                <img src={"https://image.tmdb.org/t/p/w500/xBl5AGw7HXZcv1nNXPlzGgO4Cfo.jpg"} className="rounded-xl mt-5"/>
-                <img src={"https://image.tmdb.org/t/p/w500/xBl5AGw7HXZcv1nNXPlzGgO4Cfo.jpg"} className="rounded-xl mt-5"/>
+                <img src={"https://image.tmdb.org/t/p/w500/xBl5AGw7HXZcv1nNXPlzGgO4Cfo.jpg"} className="rounded-xl mt-5" alt="recent"/>
+                <img src={"https://image.tmdb.org/t/p/w500/xBl5AGw7HXZcv1nNXPlzGgO4Cfo.jpg"} className="rounded-xl mt-5" alt="recent"/>
                 <p className="text-gray-300 text-lg w-full flex justify-end">see all &gt;</p>
             </div>
             
